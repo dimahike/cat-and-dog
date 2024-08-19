f0695cb5d02260859600d4a9dc15d9ba7e672806
dimahike <61499375+dimahike@users.noreply.github.com>
2024-08-19 18:13:35 +0300

    home page transform to SSR

--- app/breeds/cat/[id]/page.tsx
+++ app/breeds/cat/[id]/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { unstable_noStore } from "next/cache";
 
 const getCatBreeds = async (id: string) => {
   try {
@@ -20,6 +21,7 @@ const getCatBreeds = async (id: string) => {
 };
 
 async function CatDetails({ params }: { params: { id: string } }) {
+  unstable_noStore();
   const breed = await getCatBreeds(params.id);
 
   if (!breed) {
